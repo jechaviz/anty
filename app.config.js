8d561876d1e23cdfedda232fb8b836df77a10342
jechaviz <jesus.cgalaviz@gmail.com>
2025-05-28 20:49:30 -0600

    config fixes

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 export default {
   expo: {
     name: "anty",
-    slug: "anty",
+    slug: "antamina",
     // Additional Expo configuration can go here
     assetBundlePatterns: [
         "**/*"
@@ -10,9 +10,14 @@ export default {
     ios: {
       bundleIdentifier: "com.jechaviz.anty"
     },
+    android: {
+      package: "com.jechaviz.anty"
+    },
     extra: {
       eas: {
-        projectId: "d7236c21-33b9-4ed7-8a29-dcc1602f5f0b"
+        projectId: "d7236c21-33b9-4ed7-8a29-dcc1602f5f0b",
+        owner: "jechaviz",
+        userInterfaceStyle: "automatic"
       }
     }
   }
